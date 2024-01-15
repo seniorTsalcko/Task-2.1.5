eeaf078456253002929b68af8ae05c3905a962c3
seniorTsalcko <seniorTsalcko@yandex.ru>
2024-01-15 12:10:43 +0300

    refreshment

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -6,10 +6,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Application {
     public static void main(String[] args) {
+
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         KoscheiTheDeathless koscheiTheDeathless =
                 applicationContext.getBean(KoscheiTheDeathless.class);
+
         System.out.println(koscheiTheDeathless.getRulesByDeth());
     }
 }
